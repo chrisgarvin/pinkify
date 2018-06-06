3f880f8434d7509e336df255267c8b7b29adc2f2
Chris Garvin <chris.garvin@me.com>
2018-06-06 16:06:42 -0700

    use aws s3

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
 require 'sinatra'
 require 'RMagick'
 include Magick
+require 'aws-sdk-s3'
+require 'dotenv'
+Dotenv.load
 
 def get_image(name)
-  "<img src='#{path + name}.jpg' />"
-end
-
-def path(pre = nil)
-  Sinatra::Base.development? ? "#{pre}/images/" : "/tmp/"
+  "<img src='https://#{ENV['BUCKET']}.s3.amazonaws.com/#{name}' />"
 end
 
 get '/' do
@@ -18,7 +17,19 @@ post '/pinkified' do
   img = Magick::Image.read(params[:url])[0]
   @file_name = SecureRandom.uuid
   mono = img.quantize(256, Magick::GRAYColorspace)
-  pink_img = mono.level_colors('black', '#c31e62', true)
-  pink_img.write("#{path('public') + @file_name}.jpg")
+  @pink_img = mono.level_colors('black', '#c31e62', true)
+
+  Aws.config.update({
+    credentials: Aws::Credentials.new(ENV['AWS_KEY'], ENV['AWS_SECRET'])
+  })
+  client = Aws::S3::Client.new(region: 'us-west-1')
+
+  client.put_object({
+    bucket: ENV['BUCKET'],
+    key: @file_name,
+    body: @pink_img.to_blob,
+    acl: 'public-read'
+  })
+
   erb :pinkified
 end
